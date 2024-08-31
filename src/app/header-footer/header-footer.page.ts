2438a968debc6e2d008f7b612776112b1fef4bd8
Eduardo Pellegrino <162513336+eduardoCope@users.noreply.github.com>
2024-08-31 00:27:35 +0000

    header att (edu

--- src/app/header-footer/header-footer.page.ts
+++ src/app/header-footer/header-footer.page.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-header-footer',
   templateUrl: './header-footer.page.html',
   styleUrls: ['./header-footer.page.scss'],
 })
 export class HeaderFooterPage implements OnInit {
+
+
 
   constructor() { }
 
