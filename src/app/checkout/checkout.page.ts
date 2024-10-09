ffc42aa7a3b05d4d107b8eb6d0fdc84da3a72ff8
juliavaladao <162513162+juliavaladao@users.noreply.github.com>
2024-10-09 23:44:21 +0000

    ju2

--- src/app/checkout/checkout.page.ts
+++ src/app/checkout/checkout.page.ts
@@ -6,6 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkout.page.scss'],
 })
 export class CheckoutPage implements OnInit {
+  itens: any;
+
+  itens: [
+    {
+      img: '',
+      nome: 'Smash burger',
+      subpreco: 45.59,
+      preco: 91.18,
+      qtd: 2
+    }
+  ]
 
   constructor() { }
 
