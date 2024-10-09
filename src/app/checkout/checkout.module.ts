8ee8016bd3f7577b29be81274b1cd5af0cfbe3b0
juliavaladao <162513162+juliavaladao@users.noreply.github.com>
2024-10-09 23:36:23 +0000

    ju

---
+++ src/app/checkout/checkout.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { CheckoutPageRoutingModule } from './checkout-routing.module';
+
+import { CheckoutPage } from './checkout.page';
+import { HeaderModule } from '../shared/components/header/header.module';
+import { FooterModule } from '../shared/components/footer/footer.module';
+import { BtnAdicionarModule } from '../shared/components/button/btn-adicionar/btn-adicionar.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    CheckoutPageRoutingModule,
+    HeaderModule,
+    FooterModule,
+    BtnAdicionarModule
+  ],
+  declarations: [CheckoutPage]
+})
+export class CheckoutPageModule {}
