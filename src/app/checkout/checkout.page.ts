f54357e3e247590678e5d4b57313ccab7dcab876
juliavaladao <162513162+juliavaladao@users.noreply.github.com>
2024-10-10 01:30:29 +0000

    juj

--- src/app/checkout/checkout.page.ts
+++ src/app/checkout/checkout.page.ts
@@ -13,6 +13,27 @@ export class CheckoutPage implements OnInit {
       preco: 91.18,
       qtd: 2,
       icon: '../../assets/img/deletar.png'
+    },
+    {
+      img: 'https://estiloap.com.br/wp-content/uploads/2016/05/Cheeseburger-Madero.jpg',
+      nome: 'Smash burger',
+      preco: 91.18,
+      qtd: 2,
+      icon: '../../assets/img/deletar.png'
+    },
+    {
+      img: 'https://estiloap.com.br/wp-content/uploads/2016/05/Cheeseburger-Madero.jpg',
+      nome: 'Smash burger',
+      preco: 91.18,
+      qtd: 2,
+      icon: '../../assets/img/deletar.png'
+    },
+    {
+      img: 'https://estiloap.com.br/wp-content/uploads/2016/05/Cheeseburger-Madero.jpg',
+      nome: 'Smash burger',
+      preco: 91.18,
+      qtd: 2,
+      icon: '../../assets/img/deletar.png'
     }
   ]
 
