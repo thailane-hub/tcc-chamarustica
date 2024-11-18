ac0a0593033c566488f4980561b939f4dc800fed
Thailane Santos <116779272+thailane-hub@users.noreply.github.com>
2024-11-18 23:07:16 +0000

    validação senha

--- src/app/cadastro/cadastro.page.ts
+++ src/app/cadastro/cadastro.page.ts
@@ -7,14 +7,36 @@ import { AuthenticateService } from '../services/auth.service';
   styleUrls: ['./cadastro.page.scss'],
 })
 export class CadastroPage {
+  password: string = '';
+  confirmPassword: string = '';
+  errorMessage: string = '';
+ 
 
   constructor(private _authenticate:AuthenticateService
   ) { }
 
+  validarSenha() {
+    if (this.password !== this.confirmPassword) {
+      this.errorMessage = 'As senhas não coincidem.';
+    } else {
+      this.errorMessage = '';
+    }
+  }
+
  criarConta(dados: any){
-  this._authenticate.register(dados.email,dados.password)
+  this._authenticate.register(dados.email,dados.password,)
 
  }
+
+ onSubmit() {
+  this.validarSenha();
+  if (!this.errorMessage) {
+    console.log('Senhas válidas!');
+  } else {
+    console.log('Erro ao validar as senhas.');
+  }
+}
+ 
 
 //  inserirCadastrado(dados: any){
 //   const cadastrado = {
