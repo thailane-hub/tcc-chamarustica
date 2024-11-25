7abd24546298afc3e153b6e169e2388d785ffbff
juliavaladao <162513162+juliavaladao@users.noreply.github.com>
2024-11-25 23:10:00 +0000

    ok2

--- src/app/checkout/checkout.page.ts
+++ src/app/checkout/checkout.page.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProdutoService } from '../services/produto.service';
 import { CarrinhoService } from '../services/carrinho.service';
-import { OverlayEventDetail } from '@ionic/core/components';
-import { IonModal } from '@ionic/angular';
 
 @Component({
   selector: 'app-checkout',
@@ -10,26 +8,6 @@ import { IonModal } from '@ionic/angular';
   styleUrls: ['./checkout.page.scss'],
 })
 export class CheckoutPage implements OnInit {
-
-  @ViewChild(IonModal) modal: IonModal;
-
-  message = 'This modal example uses triggers to automatically open a modal when the button is clicked.';
-  name: string;
-
-  cancel() {
-    this.modal.dismiss(null, 'cancel');
-  }
-
-  confirm() {
-    this.modal.dismiss(this.name, 'confirm');
-  }
-
-  onWillDismiss(event: Event) {
-    const ev = event as CustomEvent<OverlayEventDetail<string>>;
-    if (ev.detail.role === 'confirm') {
-      this.message = `Hello, ${ev.detail.data}!`;
-    }
-  }
 
 
   constructor(
